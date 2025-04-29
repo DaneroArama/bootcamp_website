94d83edb5167c3ee4cf6df1a17fab47d34c2d8d7
DaneroArama <www.aviciiguetta@gmail.com>
2025-04-29 20:16:43 +0630

    Removal of the Merchandise

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,8 @@ const App = () => {
                     <TimetableSection />
                 </section>
 
+                <MerchandiseSection/>
+
                 <section id="sponsors">
                     <SponsorSection />
                 </section>
